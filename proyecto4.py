0d79d1b7a3a991867a4459ab2c5c750802cedd01
Valentin Quintero <vqc2500@hotmail.com>
2017-11-19 12:32:04 -0500

    Primer commit con el proyecto funcionando

---
+++ proyecto4.py
@@ -0,0 +1,59 @@
+import sys
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, CountVectorizer
+from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.clustering import KMeans
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4: # Verificar parametros
+        print("Uso: spark-submit proyecto4.py <directorioDataset> <k> <maxIteraciones>")
+        sys.exit(1)
+
+    #path = "hdfs:///user/vquinte3/examples"
+    path = sys.argv[1] #El primer parametro ingresado por consola corresponde a la ruta del directorio del dataset
+    k = int(sys.argv[2]) #El segundo parametro es el k
+    maximoIter = int(sys.argv[3]) #El tercer parametro corresponde al maximo de iteraciones
+
+
+    sc = SparkContext(appName="Proyecto4")  # SparkContext 
+    spark = SparkSession(sc)
+
+    # Lee todos los archivos dentro del directorio especificado y obtiene un RDD
+    text_files = sc.wholeTextFiles(path)
+
+    # Se convierte el RDD obtenido a un DataFrame
+    df = text_files.toDF(["Ruta", "Documento(String)"])
+
+    df.show()
+
+    # Crea los tokens que corresponden a las palabras de los documentos
+    tokenizer = Tokenizer(inputCol="Documento(String)", outputCol="tokens")
+    tokenized = tokenizer.transform(df)
+
+    tokenized.show()
+
+    remover = StopWordsRemover(inputCol="Tokens", outputCol="Tokens sin stopwords")
+    stopWordsRemoved_df = remover.transform(tokenized)
+
+    stopWordsRemoved_df.show()
+
+    hashingTF = HashingTF (inputCol="Tokens sin stopwords", outputCol="rawFeatures", numFeatures=200000)
+    tfVectors = hashingTF.transform(stopWordsRemoved_df)
+
+    tfVectors.show()
+
+    idf = IDF(inputCol="rawFeatures", outputCol="features", minDocFreq=5)
+    idfModel = idf.fit(tfVectors)
+
+    tfIdfVectors = idfModel.transform(tfVectors)
+
+    tfIdfVectors.show()
+
+    # Trains a KMeans model.
+    kmeans = KMeans().setK(k).setMaxIter(maximoIter)
+    km_model = kmeans.fit(tfIdfVectors)
+
+    clustersTable = km_model.transform(tfIdfVectors)
+
+    clustersTable.show()
